a91c2ba4c77b166d03effc0a7f9a7796518bab19
Lucas Giordano <giordano3102lucas@gmail.com>
2024-12-10 16:36:14 -0800

    add missing roles

--- notte/browser/node_type.py
+++ notte/browser/node_type.py
@@ -6,11 +6,14 @@ from typing import Callable, Required, TypedDict
 
 class NodeCategory(Enum):
     STRUCTURAL = "structural"
+    DATA_DISPLAY = "data_display"
     TEXT = "text"
     INTERACTION = "interaction"
     TABLE = "table"
     LIST = "list"
     OTHER = "other"
+    CODE = "code"
+    TREE = "tree"
     PARAMETERS = "parameters"
 
     def roles(self, add_group_role: bool = False) -> set[str]:
@@ -28,17 +31,101 @@ class NodeCategory(Enum):
                     "radio",
                     "tab",
                     "menuitem",
+                    "slider",
+                    "switch",
+                    "menuitem",
+                    "menuitemcheckbox",
+                    "menuitemradio",
                 }
             case NodeCategory.TEXT.value:
-                roles = {"text", "heading", "paragraph"}
+                roles = {
+                    "text",
+                    "heading",
+                    "paragraph",
+                    "blockquote",
+                    "caption",
+                    "contentinfo",
+                    "definition",
+                    "emphasis",
+                    "log",
+                    "note",
+                    "status",
+                    "strong",
+                    "subscript",
+                    "superscript",
+                    "term",
+                    "time",
+                }
             case NodeCategory.LIST.value:
-                roles = {"list", "listitem", "listmarker"}
+                roles = {
+                    "list",
+                    "listitem",
+                    "listmarker",
+                }
             case NodeCategory.TABLE.value:
-                roles = {"table", "row", "column", "cell"}
+                roles = {
+                    "table",
+                    "row",
+                    "column",
+                    "cell",
+                    "columnheader",
+                    "grid",
+                    "gridcell",
+                    "rowgroup",
+                    "rowheader",
+                }
             case NodeCategory.OTHER.value:
-                roles = {"iframe"}
+                roles = {
+                    "complementary",
+                    "deletion",
+                    "figure",
+                    "img",
+                    "insertion",
+                    "marquee",
+                    "meter",
+                    "presentation",
+                    "progressbar",
+                    "scrollbar",
+                    "separator",
+                    "spinbutton",
+                    "timer",
+                    "Iframe",
+                }
             case NodeCategory.STRUCTURAL.value:
-                roles = {"group", "generic", "none", "navigation", "banner", "WebArea", "dialog", "menubar"}
+                roles = {
+                    "group",
+                    "generic",
+                    "none",
+                    "application",
+                    "main",
+                    "WebArea",
+                }
+            case NodeCategory.DATA_DISPLAY.value:
+                roles = {
+                    "alert",
+                    "alertdialog",
+                    "article",
+                    "banner",
+                    "directory",
+                    "document",
+                    "dialog",
+                    "feed",
+                    "navigation",
+                    "menubar",
+                    "radiogroup",
+                    "region",
+                    "search",
+                    "tablist",
+                    "tabpanel",
+                    "toolbar",
+                    "tooltip",
+                    "form",
+                    "menu",
+                }
+            case NodeCategory.CODE.value:
+                roles = {"code", "math"}
+            case NodeCategory.TREE.value:
+                roles = {"tree", "treegrid", "treeitem"}
             case NodeCategory.PARAMETERS.value:
                 roles = {"option"}
             case _:
@@ -50,19 +137,51 @@ class NodeCategory(Enum):
 
 class NodeRole(Enum):
     # structural
-    WEBAREA = "WebArea"
-    GROUP = "group"
+    APPLICATION = "application"
     GENERIC = "generic"
+    GROUP = "group"
+    MAIN = "main"
     NONE = "none"
-    NAVIGATION = "navigation"
+    WEBAREA = "WebArea"
+
+    # Data display
+    ALERT = "alert"
+    ALERTDIALOG = "alertdialog"
+    ARTICLE = "article"
     BANNER = "banner"
+    DIRECTORY = "directory"
+    DOCUMENT = "document"
     DIALOG = "dialog"
+    FEED = "feed"
+    NAVIGATION = "navigation"
     MENUBAR = "menubar"
+    RADIOGROUP = "radiogroup"
+    REGION = "region"
+    SEARCH = "search"
+    TABLIST = "tablist"
+    TABPANEL = "tabpanel"
+    TOOLBAR = "toolbar"
+    TOOLTIP = "tooltip"
+    FORM = "form"
+    MENU = "menu"
 
     # text
     TEXT = "text"
     HEADING = "heading"
     PARAGRAPH = "paragraph"
+    BLOCKQUOTE = "blockquote"
+    CAPTION = "caption"
+    CONTENTINFO = "contentinfo"
+    DEFINITION = "definition"
+    EMPHASIS = "emphasis"
+    LOG = "log"
+    NOTE = "note"
+    STATUS = "status"
+    STRONG = "strong"
+    SUBSCRIPT = "subscript"
+    SUPERSCRIPT = "superscript"
+    TERM = "term"
+    TIME = "time"
 
     # interaction
     BUTTON = "button"
@@ -75,6 +194,10 @@ class NodeRole(Enum):
     RADIO = "radio"
     TAB = "tab"
     MENUITEM = "menuitem"
+    MENUITEMCHECKBOX = "menuitemcheckbox"
+    MENUITEMRADIO = "menuitemradio"
+    SLIDER = "slider"
+    SWITCH = "switch"
 
     # parameters
     OPTION = "option"
@@ -84,14 +207,41 @@ class NodeRole(Enum):
     ROW = "row"
     COLUMN = "column"
     CELL = "cell"
+    COLUMNHEADER = "columnheader"
+    GRID = "grid"
+    GRIDCELL = "gridcell"
+    ROWGROUP = "rowgroup"
+    ROWHEADER = "rowheader"
 
     # list
     LIST = "list"
     LISTITEM = "listitem"
     LISTMARKER = "listmarker"
 
+    # CODE
+    CODE = "code"
+    MATH = "math"
+
     # OTHER
     IFRAME = "Iframe"
+    COMPLEMENTARY = "complementary"
+    DELETION = "deletion"
+    FIGURE = "figure"
+    IMG = "img"
+    INSERTION = "insertion"
+    MARQUEE = "marquee"
+    METER = "meter"
+    PRESENTATION = "presentation"
+    PROGRESSBAR = "progressbar"
+    SCROLLBAR = "scrollbar"
+    SEPARATOR = "separator"
+    SPINBUTTON = "spinbutton"
+    TIMER = "timer"
+
+    # TREE
+    TREE = "tree"
+    TREEGRID = "treegrid"
+    TREEITEM = "treeitem"
 
     @staticmethod
     def from_value(value: str) -> "NodeRole | str":
@@ -103,7 +253,16 @@ class NodeRole(Enum):
         match self.value:
             case NodeRole.LINK.value:
                 return "L"
-            case NodeRole.BUTTON.value | NodeRole.TAB.value | NodeRole.MENUITEM.value | NodeRole.RADIO.value:
+            case (
+                NodeRole.BUTTON.value
+                | NodeRole.TAB.value
+                | NodeRole.MENUITEM.value
+                | NodeRole.RADIO.value
+                | NodeRole.CHECKBOX.value
+                | NodeRole.MENUITEMCHECKBOX.value
+                | NodeRole.MENUITEMRADIO.value
+                | NodeRole.SWITCH.value
+            ):
                 return "B"
             case (
                 NodeRole.COMBOBOX.value
@@ -112,6 +271,7 @@ class NodeRole(Enum):
                 | NodeRole.LISTBOX.value
                 | NodeRole.CHECKBOX.value
                 | NodeRole.RADIO.value
+                | NodeRole.SLIDER.value
             ):
                 return "I"
             case _:
@@ -119,7 +279,24 @@ class NodeRole(Enum):
 
     def category(self) -> NodeCategory:
         match self.value:
-            case NodeRole.TEXT.value | NodeRole.HEADING.value | NodeRole.PARAGRAPH.value:
+            case (
+                NodeRole.TEXT.value
+                | NodeRole.HEADING.value
+                | NodeRole.PARAGRAPH.value
+                | NodeRole.BLOCKQUOTE.value
+                | NodeRole.CAPTION.value
+                | NodeRole.CONTENTINFO.value
+                | NodeRole.DEFINITION.value
+                | NodeRole.EMPHASIS.value
+                | NodeRole.LOG.value
+                | NodeRole.NOTE.value
+                | NodeRole.STATUS.value
+                | NodeRole.STRONG.value
+                | NodeRole.SUBSCRIPT.value
+                | NodeRole.SUPERSCRIPT.value
+                | NodeRole.TERM.value
+                | NodeRole.TIME.value
+            ):
                 return NodeCategory.TEXT
             case NodeRole.OPTION.value:
                 return NodeCategory.PARAMETERS
@@ -128,15 +305,45 @@ class NodeRole(Enum):
                 | NodeRole.GROUP.value
                 | NodeRole.GENERIC.value
                 | NodeRole.NONE.value
-                | NodeRole.NAVIGATION.value
-                | NodeRole.BANNER.value
-                | NodeRole.DIALOG.value
-                | NodeRole.MENUBAR.value
+                | NodeRole.APPLICATION.value
+                | NodeRole.MAIN.value
             ):
                 return NodeCategory.STRUCTURAL
+            case (
+                NodeRole.ALERT.value
+                | NodeRole.ALERTDIALOG.value
+                | NodeRole.ARTICLE.value
+                | NodeRole.BANNER.value
+                | NodeRole.DIRECTORY.value
+                | NodeRole.DOCUMENT.value
+                | NodeRole.DIALOG.value
+                | NodeRole.FEED.value
+                | NodeRole.NAVIGATION.value
+                | NodeRole.MENUBAR.value
+                | NodeRole.RADIOGROUP.value
+                | NodeRole.REGION.value
+                | NodeRole.SEARCH.value
+                | NodeRole.TABLIST.value
+                | NodeRole.TABPANEL.value
+                | NodeRole.TOOLBAR.value
+                | NodeRole.TOOLTIP.value
+                | NodeRole.FORM.value
+                | NodeRole.MENU.value
+            ):
+                return NodeCategory.DATA_DISPLAY
             case NodeRole.LIST.value | NodeRole.LISTITEM.value | NodeRole.LISTMARKER.value:
                 return NodeCategory.LIST
-            case NodeRole.TABLE.value | NodeRole.ROW.value | NodeRole.COLUMN.value | NodeRole.CELL.value:
+            case (
+                NodeRole.TABLE.value
+                | NodeRole.ROW.value
+                | NodeRole.COLUMN.value
+                | NodeRole.CELL.value
+                | NodeRole.COLUMNHEADER.value
+                | NodeRole.GRID.value
+                | NodeRole.GRIDCELL.value
+                | NodeRole.ROWGROUP.value
+                | NodeRole.ROWHEADER.value
+            ):
                 return NodeCategory.TABLE
             case (
                 NodeRole.BUTTON.value
@@ -149,8 +356,16 @@ class NodeRole(Enum):
                 | NodeRole.TAB.value
                 | NodeRole.LISTBOX.value
                 | NodeRole.MENUITEM.value
+                | NodeRole.MENUITEMCHECKBOX.value
+                | NodeRole.MENUITEMRADIO.value
+                | NodeRole.SLIDER.value
+                | NodeRole.SWITCH.value
             ):
                 return NodeCategory.INTERACTION
+            case NodeRole.CODE.value | NodeRole.MATH.value:
+                return NodeCategory.CODE
+            case NodeRole.TREE.value | NodeRole.TREEGRID.value | NodeRole.TREEITEM.value:
+                return NodeCategory.TREE
             case _:
                 return NodeCategory.OTHER
 
