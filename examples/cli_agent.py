58a5bcc04e20d59dc00e6199ed055d58ea3365ee
Andrea Pinto <ap@agpinto.com>
2025-06-02 16:36:56 -0700

    fix cli agent

--- examples/cli_agent.py
+++ examples/cli_agent.py
@@ -1,15 +1,20 @@
 import typer
 from dotenv import load_dotenv
 from notte_agent import Agent
-from notte_agent.common.types import AgentResponse
 from notte_core.common.config import LlmModel
 
 # Load environment variables
 _ = load_dotenv()
 
 
-def main(headless: bool, task: str, reasoning_model: str) -> AgentResponse:
-    agent = Agent(headless=headless, reasoning_model=LlmModel(reasoning_model))
+def main(
+    task: str = typer.Option(..., help="Task to perform"),
+    reasoning_model: str = typer.Option(LlmModel.default, help="Reasoning model to use"),
+    headless: bool = typer.Option(False, help="Run in headless mode"),
+):
+    reasoning_model = LlmModel(reasoning_model) if isinstance(reasoning_model, str) else reasoning_model.default
+    headless = headless if isinstance(headless, bool) else headless.default
+    agent = Agent(headless=headless, reasoning_model=reasoning_model)
     return agent.run(task)
 
 
